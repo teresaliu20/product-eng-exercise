9888a64308f17db93d2fde08d2b6af0c4260c6fb
Teresa Liu <teresaliu20@gmail.com>
2025-02-08 13:36:47 -0500

    cleaned app.py

--- apy/app.py
+++ apy/app.py
@@ -1,32 +1,13 @@
-import math
-import os
 from cluster import cluster_feedback
 from dotenv import load_dotenv
-from flask import request
+from flask import request, Flask
 
 load_dotenv()
-import time
-
-from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def endpoint():
     post_body = request.get_json()
-
-    print('Received feedback: ' + str(len(post_body['feedback'])))
-
-    # print(post_body['feedback'])
-
     clusters = cluster_feedback(post_body['feedback'])
-    print(clusters)
-    
     return {'groupings': clusters}
-
-# @app.route('/groupings', methods=['POST'])
-# def endpoint():
-#     post_body = request.get_json()
-#     print('getting groupings')
-
-#     return {'feedback': post_body['feedback']}
